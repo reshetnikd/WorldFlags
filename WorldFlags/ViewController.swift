3fbd48324278809771a1d95d94f48a72ca2b7350
reshetnikd <dmitry.reshetnik@icloud.com>
2020-02-27 15:16:22 +0200

    Initial Commit

---
+++ WorldFlags/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldFlags
+//
+//  Created by Dmitry Reshetnik on 27.02.2020.
+//  Copyright © 2020 Dmitry Reshetnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
